191d6fee2b3ef73583d482e911cc380a261778df
Alexander Tankov <alexandartankov@gmail.com>
2015-07-11 14:37:17 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Music Library",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['pygame==1.9.1'],
+    author="Alexnadar Tankov",
+    author_email="AlexandarTankov@gmali.com",
+    description="Music-player",
+    license="MIT",
+    keywords="music, player",
+    url="https://github.com/AlexanderTankov/Music-player/"
+)
